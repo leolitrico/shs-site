ae2b513ae5919dffe72c750ba929a875df8d0d22
Awen Kidel Peña--Albert <awen.pena-albert@epfl.ch>
2023-05-30 20:36:04 +0200

    evolution fixed

--- src/components/Evolution.vue
+++ src/components/Evolution.vue
@@ -3,17 +3,13 @@
     <v-card class="elevation-5">
       <v-card-title class="headline font-weight-bold">Statistiques par Pays/Cantons de 1835 à 1895</v-card-title>
       <v-card-text>
-        <v-select
-          v-model="selectedCountry"
-          :items="countries"
-          label="Select a country"
-        ></v-select>
-        <v-select
-          v-if="selectedCountry === 'Suisse'"
-          v-model="selectedCanton"
-          :items="cantons"
-          label="Select a canton"
-        ></v-select>
+        <v-select v-model="selectedCountry" :items="countries" label="Select a country"></v-select>
+        <div v-if="selectedCountry === 'Suisse'">
+          <v-select v-model="selectedCanton" :items="cantons" label="Select a canton"></v-select>
+        </div>
+        <div class="image-container" v-if="(selectedCountry && selectedCountry !== 'Suisse') || (selectedCountry === 'Suisse' && selectedCanton)">
+          <img :src="imageSrc" :alt="selectedCountry || selectedCanton" :title="selectedCountry || selectedCanton" />
+        </div>
       </v-card-text>
     </v-card>
   </v-container>
@@ -26,9 +22,49 @@ export default {
     return {
       selectedCountry: null,
       selectedCanton: null,
-      countries: ['France', 'Allemagne', 'Italie', 'Suisse', 'Angleterre', 'Prusse', 'Russie', 'Hollande', 'Roumanie', 'Autriche', 'Amerique', 'Suede', 'Bulgarie', 'Espagne', 'Mali', 'Australie', 'Belgique', 'Maroc', 'Grece', 'Pologne', 'Ecosse', 'Danemark', 'Jamaique', 'Canada', 'Luxembourg', 'Moldavie', 'Argentine', 'Indonésie', 'Turquie', 'Algerie', 'Japon', 'Bolivie', 'Egypte', 'Armenie', 'Inde'],
-      cantons: ['Vaud', 'Valais', 'Fribourg', 'Genève', 'Neuchâtel', 'Jura', 'Berne', 'Zurich', 'Lucerne', 'Grisons', 'Tessin', 'Uri', 'Schwytz', 'Obwald', 'Nidwald', 'Glaris', 'Appenzell Rhodes-Extérieures', 'Appenzell Rhodes-Intérieures', 'Saint-Gall', 'Argovie', 'Thurgovie', 'Bâle-Campagne', 'Bâle-Ville', 'Zoug']
+      countries: ['Suisse', 'Allemagne', 'Amerique', 'Angleterre', 'Autriche', 'Bulgarie', 'Danemark', 'Ecosse', 'Espagne', 'France', 'Hollande', 'Italie', 'Moldavie', 'Prusse', 'Roumanie', 'Russie', 'Suede'],
+      cantons: ['Argovie', 'Bale', 'Berne', 'Fribourg', 'Geneve', 'Glaris', 'Grisons', 'Jura', 'Lucerne', 'Neuchatel', 'SaintGall', 'Soleure', 'Tessin', 'Thurgovie', 'Valais', 'Zurich'],
     }
-  }
+  },
+  computed: {
+    imageSrc() {
+      if (this.selectedCountry) {
+        if (this.selectedCountry === 'Suisse') {
+          if (this.selectedCanton) {
+            const cantonSlug = this.selectedCanton.toLowerCase().replace(/ /g, '_');
+            return this.getImageUrl(`evolution_cantons/evolution_${cantonSlug}.png`);
+          } else {
+            return null;
+          }
+        } else {
+          const countrySlug = this.selectedCountry.toLowerCase().replace(/ /g, '_');
+          return this.getImageUrl(`evolution_pays/evolution_${countrySlug}.png`);
+        }
+      } else {
+        return null;
+      }
+    },
+  },
+  methods: {
+    getImageUrl(imageName) {
+      return require(`@/assets/${imageName}`);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.image-container {
+  max-width: 100%;
+  max-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-container img {
+  max-width: 80%;
+  max-height: 80%;
+  margin-top: -45px;
+}
+</style>
